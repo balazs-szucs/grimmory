640db293c02d8a3fd89b5f42c3023c9ee61d4047
Balázs Szücs <bszucs1209@gmail.com>
2026-04-03 21:52:01 +0200

    chore(deps): update epub4j dependency to version 1.0.1

--- booklore-api/build.gradle.kts
+++ booklore-api/build.gradle.kts
@@ -127,7 +127,7 @@ dependencies {
     implementation("com.twelvemonkeys.imageio:imageio-bmp:3.13.1")
 
     // epub4j-grimmory fork publishes as org.grimmory:epub4j-core
-    val epub4jCoords = if (useLocalLibs) "org.grimmory:epub4j-core:+" else "io.documentnode:epub4j-core:4.2.3"
+    val epub4jCoords = if (useLocalLibs) "org.grimmory:epub4j-core:+" else "org.grimmory:epub4j-core:1.0.1"
     implementation(epub4jCoords)
 
     // --- Audio Metadata (Audiobook Support) ---
